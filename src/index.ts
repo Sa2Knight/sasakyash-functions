c469ecd1505dd419754694a6a3a7ecf00697a68a
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2020-12-07 03:45:51 +0900

    キャッシュ破棄を24時間に1回行うようにする

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,13 @@ import monthTotalPaymentAmountAPI from './api/monthTotapPaymentAmount'
 import DailyPaymentAmountsAPI from './api/dailyPaymentAmount'
 import ClearCache from './api/clearCache'
 import * as functions from 'firebase-functions'
+import { Collection } from './store'
 
 export const monthTotalPaymentAmount = monthTotalPaymentAmountAPI
 export const dailyPaymentAmounts = DailyPaymentAmountsAPI
 export const clearCache = ClearCache
 
-exports.scheduledFunction = functions.pubsub.schedule('every 1 minutes').onRun(context => {
-  console.log('this will be run every 1 minutes!!')
+// 24時間に1回、キャッシュを破棄する
+exports.scheduledFunction = functions.pubsub.schedule('every 24 hours').onRun(context => {
+  return new Collection('zaim-get-money-cache').clear()
 })
