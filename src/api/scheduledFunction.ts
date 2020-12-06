ab050fa15604744633ddb0f424d6e2e999dee380
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2020-12-07 03:39:49 +0900

    scheduledFunction のテストをexport

--- src/api/scheduledFunction.ts
+++
@@ -1,5 +0,0 @@
-import * as functions from 'firebase-functions'
-
-exports.scheduledFunction = functions.pubsub.schedule('every 1 minutes').onRun(context => {
-  console.log('this will be run every 1 minutes!!')
-})
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import monthTotalPaymentAmountAPI from './api/monthTotapPaymentAmount'
 import DailyPaymentAmountsAPI from './api/dailyPaymentAmount'
 import ClearCache from './api/clearCache'
+import * as functions from 'firebase-functions'
 
 export const monthTotalPaymentAmount = monthTotalPaymentAmountAPI
 export const dailyPaymentAmounts = DailyPaymentAmountsAPI
 export const clearCache = ClearCache
+
+exports.scheduledFunction = functions.pubsub.schedule('every 1 minutes').onRun(context => {
+  console.log('this will be run every 1 minutes!!')
+})
