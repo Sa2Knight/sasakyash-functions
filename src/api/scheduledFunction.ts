8c195ca56a72f7fbc935a30feda5b02245693884
shingo.sasaki <shingo.sasaki.0529@gmail.com>
2020-12-07 03:28:28 +0900

    未使用のimportを削除

--- src/api/scheduledFunction.ts
+++ src/api/scheduledFunction.ts
@@ -1,5 +1,4 @@
 import * as functions from 'firebase-functions'
-import { Collection } from '../store'
 
 exports.scheduledFunction = functions.pubsub.schedule('every 1 minutes').onRun(context => {
   console.log('this will be run every 1 minutes!!')
